4fc6649350f2a11ca6dcd5272a1434d4486201e2
haidara910 <haidaraslyman473@gmail.com>
2025-05-25 22:45:29 +0330

    Add files via upload

---
+++ song.h
@@ -0,0 +1,31 @@
+﻿#ifndef SONG_H
+#define SONG_H
+
+#include <string>
+using namespace std;
+
+class Song {
+private:
+    string name;
+    string singer;
+    int year;
+    string genre;
+
+public:
+    Song();
+    Song(string name, string singer, int year, string genre);
+
+    void setName(string name);
+    void setSinger(string singer);
+    void setYear(int year);
+    void setGenre(string genre);
+
+    string getName() const;     
+    string getSinger() const;   
+    int getYear() const;        
+    string getGenre() const;    
+
+    void printInfo() const;     
+};
+
+#endif
